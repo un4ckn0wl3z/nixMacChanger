c71ae11e4a5173efac054102e45dd1adfb496b57
devinflydrop <psynoxzhio@rmutsvmail.com>
2018-07-22 10:18:56 +0700

    add code template

---
+++ main_mac_changer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Created by un4ckn0wl3z-level99
+# Website -> www.un4ckn0wl3z.xyz
+# Date -> 7/22/2018
+
+import subprocess
+
+interface = raw_input("interface> ")
+new_mac = raw_input("new MAC> ")
+
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig",interface,"hw","ether", new_mac])
+subprocess.call(["ifconfig",interface,"up"])
+
+
+
+
+
+
+
